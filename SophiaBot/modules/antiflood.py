0311bc0416657082d511cc6a7c5a902f015581c2
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-10-31 08:44:26 +0530

    Update antiflood.py

--- SophiaBot/modules/antiflood.py
+++ SophiaBot/modules/antiflood.py
@@ -425,7 +425,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "AntiFlood"
+__mod_name__ = "🧪Anti Flood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
