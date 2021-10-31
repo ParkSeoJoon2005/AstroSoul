797fd39c06afa151d017f4c47388881433b75fcb
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-10-31 09:18:20 +0530

    Update chatbot.py

--- SophiaBot/modules/chatbot.py
+++ SophiaBot/modules/chatbot.py
@@ -1,15 +1,16 @@
 __help__ = """
+⚊❮❮❮❮ ｢**HELP FOR  AI👤 MODULE** 」❯❯❯❯⚊
 <b> Chatbot </b>
-Sophia AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+Aurora AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
  - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
  
  
 <b> Assistant </b>
- - /ask [question]: Ask question from daisy
+ - /ask [question]: Ask question from Aurora
  - /ask [reply to voice note]: Get voice reply
  
 
 """
 
-__mod_name__ = "AI Assistant"
+__mod_name__ = "Chat Bot👤"
