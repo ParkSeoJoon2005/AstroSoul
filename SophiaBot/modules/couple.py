2812bfb338427699638e9a2dfb19dc2f1231c78d
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-10-31 09:23:54 +0530

    Update couple.py

--- SophiaBot/modules/couple.py
+++ SophiaBot/modules/couple.py
@@ -95,8 +95,10 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 
 
 __help__ = """
-- /couples - To Choose Couple Of The Day ❤
 
- @SophiaUpdates 
+⚊❮❮❮❮ ｢**HELP FOR Couple❤  MODULE**」❯❯❯❯⚊
+- /couples - To Choose Couple Of The Day❤
+
+  
  """
-__mod_name__ = "Couples"
+__mod_name__ = "Couples❤"
